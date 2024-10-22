dc48a6daaa429deba1cb72fa8b0a5489524ef47f
_erfanvahdat_ <erfanvahdat.6@gmail.com>
2024-10-22 16:03:53 +0330

    export cosnt name has changed

---
+++ src/mongoose/schemas/Trade_status.mjs
@@ -0,0 +1,46 @@
+
+
+import mongoose, { mongo } from "mongoose";
+
+const Trade = new  mongoose.Schema({
+    symbol : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: true
+        
+    },
+    stop_loss : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: false
+        
+    },
+    take_profit : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: false
+    },
+
+    limitprice : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: false
+        
+    },
+    quantity : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: false
+    },
+    side : {
+        type:mongoose.Schema.Types.String,
+            require :true,
+            unique: false
+        
+    }
+    
+})
+
+const Trade_status  =  mongoose.model('Trade',Trade)
+
+export default Trade_status;
